553ca0a2b0e08d4cf3ca42054c8f3c8b0b3808ea
Akash <akash01algo@gmail.com>
2025-03-08 23:50:08 +0600

    Update Main.jsx

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router";
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-
 const Main = () => {
     console.log('Main component rendered');
     return (
